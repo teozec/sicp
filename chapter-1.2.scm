f7edafcd57d7d6b741c97700dac9e1c663b31a7a
Matteo Zeccoli Marazzini <teo.zec@hotmail.it>
2025-04-20 17:20:16 +0200

    feat: add sections 1.2.5-1.2.6, exercises 1.20-1.28

--- chapter-1.2.scm
+++ chapter-1.2.scm
@@ -221,7 +221,10 @@
   (iter a b 0))
 
 ; ---- Exercise 1.19
-(define (fib b)
+; T_pq can be represented by the matrix (p+q p, q p), since T_pq(a, b) = (qa+pa+qb, qa+pb).
+; Furthermore, by matrix multiplication T_pq^2 = (2q^2+p^2+2pq q^2+2pq, q^2+2pq q^2+p^2),
+; which is T_p'q' if p'=q^2+2pq and q'=q^2+p^2.
+(define (fib n)
   (define (iter a b p q count)
     (cond ((= count 0) b)
 	  ((even? count)
@@ -234,4 +237,197 @@
 		      (+ (* b p) (* a q))
 		      p
 		      q
-		      (-1+ count))))))
+		      (-1+ count)))))
+  (iter 1 0 0 1 n))
+
+
+; Section 1.2.5
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+; ---- Exercise 1.20
+; Using applicative order, we calculate the remainder at each step. Since 4 steps are needed, we compute remainder 4 times.
+; Using normal order, we delay the calculation of the 4 remainders at the end of the process.
+; However, at every step we need to evaluate b to check the condition of the if special form, and in turn b will be an expression containing a certain number of remainders, increasing at every step
+; (since we compute it in the condition but not in the consequent when we do the recursive call.
+; Tracing the process, we see that in the 4 setps we call remainder 1, 2, 4 and 7 times. Adding the final 4 calls in the last consequent, we see that remainders has been called 18 times.
+
+; Section 1.2.6
+
+; Testing fpr primality, method 1: searching for divisors. T = \Theta(n)
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (1+ test-divisor)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+; Testing for primality, method 2: Fermat test. Probabilistic, T = \Theta(lg(n))
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+	((even? exp)
+	 (remainder (square (expmod base (/ exp 2) m))
+		    m))
+	(else
+	 (remainder (* base (expmod base (-1+ exp) m))
+		    m))))
+
+(define (fermat-test n a)
+    (= (expmod a n n) a))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+	((fermat-test n (1+ (random (-1+ n))))
+	 (fast-prime? n (-1+ times)))
+	(else false)))
+
+; ---- Exercise 1.21
+(smallest-divisor 199)   ; 199
+(smallest-divisor 1999)  ; 1999
+(smallest-divisor 19999) ; 7
+
+; ---- Exercise 1.22
+(define (timed-prime-test n)
+  (define (start-prime-test n start-time)
+    (if (prime? n)
+	(report-prime (- (runtime) start-time))
+	false))
+  
+  (define (report-prime elapsed-time)
+    (display " **** ")
+    (display elapsed-time)
+    true)
+  
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (search-for-primes a n)
+  (if (not (= n 0))
+      (cond ((even? a) (search-for-primes (1+ a) n))
+	    ((timed-prime-test a) (search-for-primes (+ 2 a) (-1+ n)))
+	    (else (search-for-primes (+ 2 a) n)))))
+; Since modern computers are faster, I needed some more digits...
+(search-for-primes 100000000 3)   ; 0.04 s / sqrt(100000000)   = 4e-6
+(search-for-primes 1000000000 3)  ; 0.16 s / sqrt(1000000000)  = 5e-6
+(search-for-primes 10000000000 3) ; 0.52 s / sqrt(10000000000) = 5e-6
+; The ratio between the time and the sqrt of the steps is roughly constant, as expected.
+
+; ---- Exercise 1.23
+(define (next test-divisor)
+  (if (= test-divisor 2)
+      3
+      (1+ test-divisor)))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+	((divides? test-divisor n) test-divisor)
+	(else (find-divisor n (next test-divisor)))))
+
+(search-for-primes 100000000 3)   ; 0.06 s
+(search-for-primes 1000000000 3)  ; 0.21 s
+(search-for-primes 10000000000 3) ; 0.80 s
+; The time does not decrease, it actually increases a little (considering variations).
+; More testing is needed.
+; I do expect a speedup, not of a factor 2, because while removing half of the steps, we added a funcion call and an additional if clause.
+
+; ---- Exercise 1.24
+(define (timed-fast-prime-test n)
+  (define (start-prime-test n start-time)
+    (if (fast-prime? n 1000)
+	(report-prime (- (runtime) start-time))
+	false))
+  
+  (define (report-prime elapsed-time)
+    (display " **** ")
+    (display elapsed-time)
+    true)
+  
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (search-for-fast-primes a n)
+  (if (not (= n 0))
+      (cond ((even? a) (search-for-fast-primes (1+ a) n))
+	    ((timed-fast-prime-test a) (search-for-fast-primes (+ 2 a) (-1+ n)))
+	    (else (search-for-fast-primes (+ 2 a) n)))))
+
+(search-for-fast-primes 100000000 3)   ; 0.27 s / lg_10(100000000)   = 0.034
+(search-for-fast-primes 1000000000 3)  ; 0.32 s / lg_10(1000000000)  = 0.035
+(search-for-fast-primes 10000000000 3) ; 0.37 s / lg_10(10000000000) = 0.037
+; The ratio between the time and the lg of the steps is roughly constant, as expected.
+
+; ---- Exercise 1.25
+(define (bad-expmod base exp m)
+  (remainder (fast-expt-iter base exp) m))
+; The difference between expmod and bad-expmod is that bad-expmod needs to compute the exponentiation result first.
+; This result may be very large, and its calculation involves operation with large numbers.
+; In theory, the operation is \Theta(lg(n)), but in the CPU operations with very large numbers may not be natively supported, requiring algorothms that may increase the complexity.
+; expmod, on the other hand, aways deals with small numbers, computing the reminder at every step.
+
+; ---- Exercise 1.26
+; Without using the square function, in applicative order, the (expmod base (/ exp 2) m) needs to be computed twice at every step (when exp is even).
+; This means that, if T(n) is the number of steps required to calculate (expmod base n m), then T(2n) = 2T(n) + k, meaning that T(n) = \Theta(n).
+; Conversely, by using square we need to calculate (expmod base (/ exp 2) m) only once, thus T(2n) = T(n) + k, and T(n) = \Theta(lg(n)).
+
+; ---- Exercise 1.27
+(define (fermat-full n)
+  (define (iter a)
+    (cond ((= a n) true)
+	  ((fermat-test n a) (iter (1+ a)))
+	  (else false)))
+  (iter 1))
+(fermat-full 19) ; True, as expected since 19 is prime.
+(fermat-full 10) ; False, as expeced since 10 is not prime.
+; the following are Carmichael numbers: they pass the test (the result is true) but they are not primes.
+(fermat-full 561)
+(fermat-full 1105)
+(fermat-full 1729)
+(fermat-full 2465)
+(fermat-full 2821)
+(fermat-full 6601)
+
+; ---- Exercise 1.28
+
+(define (miller-rabin-test n a)
+  (define (checked x)
+    (if (= x 1)
+	0
+	x))
+
+  (define (checked-square-remainder n m)
+    (if (or (= n 1)
+	    (= n (-1+ m)))
+	(remainder (square n) m)
+	(checked (remainder (square n) m))))
+  
+  (define (checked-expmod base exp m)
+    (cond ((= exp 0) 1)
+	  ((even? exp)
+	   (checked-square-remainder
+	    (checked-expmod base (/ exp 2) m)
+	    m))
+	  (else
+	   (remainder (* base (expmod-miller-rabin base (-1+ exp) m))
+		      m))))
+  
+  (not (= (checked-expmod a (-1+ n) n) 0)))
+
+(define (miller-rabin-prime? n)
+  (define (iter step)
+    (cond ((> step (1+ (/ n 2))) true)
+	  ((miller-rabin-test n step) (iter (+ 1 step)))
+	  (else false)))
+  (cond	((= n 2) true)
+	((even? n) false)
+	(else (iter 2))))
